bf7a9ae6dc82d4094b87529fe5e342b39bdd21aa
root <root@DESKTOP-RHA5CJR.localdomain>
2024-07-14 14:37:04 -0700

    scope lab submit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    let customername2 = customerName.toUpperCase();
+    return customername2;
+}
+
+function setBestCustomer(){
+
+}
+
+function overwriteBestCustomer(){
+
+}
+
+const leastFavoriteCustomer = x;
+function changeLeastFavoriteCustomer(){
+    
+}
